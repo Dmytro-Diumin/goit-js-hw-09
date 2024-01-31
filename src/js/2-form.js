991d630c82669c79a20731209a10c29536b49e2d
Dmytro-Diumin <microb201196@gmail.com>
2024-02-01 01:05:48 +0200

    update 2-form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -13,16 +13,27 @@ function onFormInput() {
     localStorage.setItem("feedback-form-state", JSON.stringify(userData));
 }
 
+function userDataMessage() {
+    const userDataMessageRef = JSON.parse(localStorage.getItem("feedback-form-state"));
+    if(userDataMessageRef) {
+        emailRef.value = userDataMessageRef.email.trim();
+        messageRef.value = userDataMessageRef.message.trim();
+    }
+}
+
 formRef.addEventListener("submit", onFormSubmit);
 
 function onFormSubmit(e) {
     e.preventDefault();
-
-    if (emailRef.value.trim() === '' || messageRef.value.trim() === '') {
+    
+    if (emailRef.value !== '' && messageRef.value !== '') {
+        console.log({
+            email: emailRef.value.trim(),
+            message: messageRef.value.trim(),
+        });
+        e.currentTarget.reset();
+        localStorage.removeItem("feedback-form-state");
+      } else {
         alert('Please enter your email and message');
-        return;
       }
-    console.log("Data submit:", JSON.parse(localStorage.getItem("feedback-form-state")));
-    e.currentTarget.reset();
-    localStorage.removeItem("feedback-form-state");
 }
